68ad5f621102112367f7f17701741d7d8f5e501d
muhammad.arqum85@gmail.com <muhammad.arqum85@gmail.com>
2018-09-07 03:01:01 +0300

    2

---
+++ src/Components/aboutComponent/about.js
@@ -0,0 +1,5 @@
+import React, {Component} from 'react';
+import $ from 'jquery';
+import MainNavigation from "../navigationComponent/mainNavigation";
+import Cursor from "../cursorComponent/cursor";
+
