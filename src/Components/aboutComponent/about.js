6ae29d0e6ca80cd36c5e03d4ad2ab9fc44d261aa
muhammad.arqum85@gmail.com <muhammad.arqum85@gmail.com>
2018-09-23 07:03:28 +0300

    23

--- src/Components/aboutComponent/about.js
+++ src/Components/aboutComponent/about.js
@@ -75,67 +75,10 @@ componentWillMount(){
 
            }
        },600);
+    }
 
      
 
-
-// GRADIENT TRANSITION
-   (function () {
-    var colors = new Array(
-        [44, 51, 67], [40, 47, 64], [45, 54, 72], [44, 47, 61]);
-
-    var step = 0;
-    var colorIndices = [0, 1, 2, 3];
-
-    //transition speed
-    var gradientSpeed = 0.003;
-
-    function updateGradient() {
-
-        if ($ === undefined) return;
-
-        var c0_0 = colors[colorIndices[0]];
-        var c0_1 = colors[colorIndices[1]];
-        var c1_0 = colors[colorIndices[2]];
-        var c1_1 = colors[colorIndices[3]];
-
-        var istep = 1 - step;
-        var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
-        var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
-        var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-        var color1 = "rgb(" + r1 + "," + g1 + "," + b1 + ")";
-
-        var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
-        var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
-        var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-        var color2 = "rgb(" + r2 + "," + g2 + "," + b2 + ")";
-
-        $('#ab_gradient').css({
-            // background: "-webkit-gradient(linear, left top, right top, from(" + color1 + "), to(" + color2 + "))"
-            background: "linear-gradient(to bottom," + color1 + "," + color2 + ")"
-        }).css({
-            background: "-moz-linear-gradient(to bottom, " + color1 + " 0%, " + color2 + " 100%)"
-        });
-
-       
-
-        step += gradientSpeed;
-        if (step >= 1) {
-            step %= 1;
-            colorIndices[0] = colorIndices[1];
-            colorIndices[2] = colorIndices[3];
-
-            //pick two new target color indices
-            //do not pick the same as the current one
-            colorIndices[1] = (colorIndices[1] + Math.floor(1 + Math.random() * (colors.length - 1))) % colors.length;
-            colorIndices[3] = (colorIndices[3] + Math.floor(1 + Math.random() * (colors.length - 1))) % colors.length;
-
-        }
-    }
-
-    intervelll = setInterval(updateGradient, 10);
-})();
-    }
 
 
     componentWillUnmount() {
@@ -217,12 +160,12 @@ There are many variations of passages of Lorem Ipsum available, but the majority
 
 <ScrollAnimation animateIn='fadeInUp' animateOut='fadeOutUp' duration={1}>
 
-<div class="scroll-downs animated fadeInUp" onClick={()=> { scrollToComponent(this.refs.bottom, {
+<div class="scroll-downs " onClick={()=> { scrollToComponent(this.refs.bottom, {
                 offset: 0,
                 align: 'top',
-                duration: 3000
+                duration: 600
             })}}>
-  <div class="mousey">
+  <div class="mousey animated fadeInUp">
     <div class="scroller"></div>
   </div>
 </div>
@@ -250,9 +193,10 @@ There are many variations of passages of Lorem Ipsum available, but the majority
 
 
 <div className="leftBorder col-md-3 col-sm-12 nopadding rightBorder">
+
 <ScrollAnimation animateIn='fadeInUp' animateOut='fadeOutDown' duration={1}>
 <div className="cw_txt" id="ab_gradient" >
-<p className="inner-paragraph animated fadeIn delay-1s">
+<p className="inner-paragraph">
 There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
 </p>
 </div>
@@ -264,7 +208,7 @@ There are many variations of passages of Lorem Ipsum available, but the majority
 
 
 <div className="col-md-3 col-sm-12 rightBorder">
-<ScrollAnimation animateIn='fadeInUp' animateOut='fadeOutDown'>
+<ScrollAnimation animateIn='fadeIn'>
 <div className="cw_img" ref="bottom"></div>
 </ScrollAnimation>
 
